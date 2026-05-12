56db5fe4bab9fd24826a1fcc0219242f5621a2e0
rychh <rych2@mail.ru>
2026-05-12 22:52:20 +0300

    Добавьте файлы проекта.

---
+++ TestJobService/LogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJobService
+{
+    public class LogModel
+    {
+        public string? HostName { get; set; }
+        public string? IP {  get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int? CpuLoad { get; set; }
+        public string? MemoryUsage { get; set; }
+        public string? DiskUsage { get; set; }
+    }
+}
---
+++ TestJobService/SystemInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJobService
+{
+    internal static class SystemInfo
+    {
+        internal static LogModel GetInfo(CancellationToken stoppingToken) 
+        {
+            LogModel ret = new LogModel();
+            ret.HostName = Dns.GetHostName();
+            ret.IP = Dns.GetHostAddresses(ret.HostName).FirstOrDefault(x=>x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?.ToString(); // Берём только IPv4 при необходимости можно переписать для IPv6 или писать оба
+            ret.DateTime = DateTime.Now;
+
+            ret.GetSysInfo();
+
+            return ret;
+        }
+
+        public static void GetSysInfo(this LogModel model)
+        {
+            var searcher = new ManagementObjectSearcher("SELECT LoadPercentage FROM Win32_Processor");
+
+            if(int.TryParse(searcher.Get().OfType<ManagementObject>()?.FirstOrDefault()?["LoadPercentager"].ToString(), out int res))
+                model.CpuLoad = res;
+        }
+    }
+}
